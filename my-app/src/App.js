0b61c2e796ac684908d448eb7049578c82e9175f
gaserashraf <elkinggaser@gmail.com>
2022-11-14 13:04:08 +0200

    add navbar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Fragment } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
+import PrivateRoute from './routing/PrivateRoute'
+import Home from "./components/home/Home";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+import NotFound from "./components/notFound/NotFound";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar />
+        <div className="container">
+          <Routes>
+            {/* <PrivateRoute exact path="/" element={Home} /> */}
+            <Route exact path="/register" element={Register} />
+            <Route exact path="/login" element={Login} />
+            <Route exact element={NotFound} />
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
---
+++ my-app/src/components/navbar/Navbar.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+
+const Navbar = () => {
+  const [mobileView, setMobileView] = useState(false);
+
+  const toogleMobileIcon = () => setMobileView(!mobileView);
+  const gusetLinks = (
+    <>
+      <li className="nav-item">
+        <Link className="nav-links" to="/register" onClick={()=>setMobileView(false)}>
+          Regsiter
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link className="nav-links" to="/login" onClick={()=>setMobileView(false)}>
+          Login
+        </Link>
+      </li>
+    </>
+  );
+
+  return (
+    <nav class="navbar navbar-expand-lg navbar-light bg-light justify-content-between">
+      <h1>
+        <i className="fas fa-trophy mr-1"></i>
+        Qatar 2022
+      </h1>
+
+      <div class="justify-content-end">
+        <button class="navbar-toggler" onClick={toogleMobileIcon}>
+          <i
+            className={`text-dark ${
+              mobileView ? "fas fa-times" : "fas fa-bars"
+            }`}
+          />
+        </button>
+        <ul className={mobileView ? "nav-menu active" : "nav-menu"}>
+          {gusetLinks}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
