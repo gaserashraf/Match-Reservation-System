44a09cf6e98ce57d39bc54f237b1889d4cbc9ce8
We2Am-BaSsem <58189568+We2Am-BaSsem@users.noreply.github.com>
2022-12-07 12:53:36 +0200

    init node js server + mongo db connection

---
+++ server/index.js
@@ -0,0 +1,14 @@
+// initiate a server and run it on port 3000 
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const DB_URI = "mongodb://localhost/MatchReservationsSystem";
+require('./db/mongoose')(DB_URI);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+// initiate a server and run it on port 3000    
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
