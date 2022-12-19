749373b9c4e952fdaa56a2e7f6dca3941046ef8a
BigFish2086 <a8686.ibrahim@gmail.com>
2022-12-20 01:28:01 +0200

    add some helper functions and error/success messages

---
+++ backend/src/app/Http/Misc/Helpers/Success.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Misc\Helpers;
+
+/**
+ *  external resource from https://github.com/SiliconArena/alphamart-backend
+ */
+class Success
+{
+  // user_success_messages
+  const UPDATED = "Record updated successfully!";
+  const DELETED = "Record deleted successfully!";
+  const ADDED = "Record added successfully!";
+  const CREATED = "Record created successfully!";
+  const SENT = "Email sent successfully!";
+  const CHANGE = "password change successfully!";
+
+  const STILL_NOT_ALLOWED = "You are successfully registered but still not allowed to login. Admin will allow you soon.";
+}
