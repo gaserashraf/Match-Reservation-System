25226e2b931b41e2fe830c845e39f0e1c18f4dc2
BigFish2086 <a8686.ibrahim@gmail.com>
2022-12-27 15:13:59 +0200

    add admin user from env

--- backend/src/database/seeders/UserSeeder.php
+++ backend/src/database/seeders/UserSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -15,11 +16,22 @@ class UserSeeder extends Seeder
   public function run()
   {
     // create 3 verified admin users
-    User::factory()->count(3)->create([
+    try {
+      User::factory()->count(1)->create([
+        'role' => 0,
+        'allowed' => 1,
+        'email' => env("TEMP_ADMIN_EMAI"),
+        'password' => Hash::make(env("TEMP_ADMIN_PASS")),
+        'email_verified_at' => now(),
+      ]);
+    } catch (\Exception $e) {
+    }
+    User::factory()->count(2)->create([
       'role' => 0,
       'allowed' => 1,
       'email_verified_at' => now(),
     ]);
+
 
     User::factory()->count(2)->create();
   }
